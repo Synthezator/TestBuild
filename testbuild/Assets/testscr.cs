909f324188d756dd303e1af28c2cd4d508e4930f
Pavel Leonenko <pavel.leonenko0@gmail.com>
2017-02-06 22:16:07 +0300

    Initial commit.

---
+++ testbuild/Assets/testscr.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testscr : MonoBehaviour {
+	public Transform cube;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void moveleft() {
+		cube.position += Vector3.left;
+
+	
+	}
+	public void moveright() {
+		cube.position += Vector3.right;
+	
+	}
+}
